4eacc71e8be8e6b7ac87b7aa86a5ed91e2210803
Nikolj <frededdd@mail.ru>
2022-05-21 14:45:59 +0300

    feat: add tast 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,33 @@
+const fs = require('fs');
+const fsPromises = fs.promises;
+const path = require('path');
+const fromCopy = path.join(__dirname, 'files');
+const toCopy = path.join(__dirname, 'files-copy');
 
+fs.access(toCopy, (error) => {
+  if (error) {
+    fsPromises.mkdir(toCopy);
+    console.log('Каталог files-copy создан');
+  } else {
+    console.log('Каталог files-copy существует');
+  }
+});
+
+async function copyDir(fromPath, toPath) {
+  await fsPromises.rm(toPath, { force: true, recursive: true });
+  await fsPromises.mkdir(toPath, { recursive: true });
+  const filesNameArr = await fsPromises.readdir(fromPath, { withFileTypes: true });
+  for (let item of filesNameArr) {
+    const currentItemPath = path.join(fromPath, item.name);
+    const copyItemPath = path.join(toPath, item.name);
+
+    if (item.isDirectory()) {
+      await fsPromises.mkdir(copyItemPath, { recursive: true });
+      await copyDir(currentItemPath, copyItemPath);
+    } else if (item.isFile()) {
+      await fsPromises.copyFile(currentItemPath, copyItemPath);
+    }
+  }
+}
+
+copyDir(fromCopy, toCopy);
